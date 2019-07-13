5761c5a3c82a39def2d3b8ecf81f490ef04e8011
Bladimir Orellana <me@bladimirorellana.com>
2019-07-12 23:03:17 -0500

    Added sound

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,33 +13,36 @@ var defender = "";
 var counterwin = 0;
 var line1 = $("<div>");
 var line2 = $("<div>");
-var powerAttack = Math.floor(Math.random() * 20 + 1);
+var powerAttackValue = Math.floor(Math.random() * 20 + 1);
+var counterAttackValue = Math.floor(Math.random() * 20 + 1);
 
 
 
 var maul = {
     name: "maul",
     healthPoints: 100,
-    attackPower: powerAttack,
-    counterAttackPower: 10,
+    attackPower: powerAttackValue,
+    counterAttackPower: counterAttackValue,
+    
 };
+console.log(powerAttackValue)
 var luke = {
     name: "luke",
     healthPoints: 100,
-    attackPower: powerAttack,
-    counterAttackPower: 5,
+    attackPower: powerAttackValue,
+    counterAttackPower: counterAttackValue,
 };
 var obi = {
     name: "obi",
     healthPoints: 100,
-    attackPower: powerAttack,
-    counterAttackPower: 20,
+    attackPower: powerAttackValue,
+    counterAttackPower: counterAttackValue,
 };
 var mace = {
     name: "mace",
     healthPoints: 100,
-    attackPower: powerAttack,
-    counterAttackPower: 25,
+    attackPower: powerAttackValue,
+    counterAttackPower: counterAttackValue,
 };
 
 
@@ -197,14 +200,13 @@ var mace = {
             if (playerHealthpoints > 0 && enemyHealthpoints > 0) {
                 line1.text("You attacked " + enemyname + " for " + playerAttackUpdate + " damage");
                 $("#defenderDiv").append(line1);
-                line2.text(enemyname + " attacked you back for " + enemyCounterAttack + " damage");
+                line2.text(enemyname + " attacked you back for " + counterAttackValue + " damage");
                 $("#defenderDiv").append(line2);
             }
-            else if (playerHealthpoints === 0) {
+            else if (playerHealthpoints <= 0) {
                 $("#attack").attr("disabled", true);
                 line1.text("You've been defeated...GAME OVER!!!");
                 $("#defenderDiv").append(line1);
-                //$("#defenderDiv").append(resetbutton);
                 $("#resetbutton").css("visibility", "visible");
                 line2.empty();
             }
@@ -239,68 +241,7 @@ var mace = {
         $("#resetbutton").hide();
         location.reload();
 
-       /* line1.empty();
-        playerselected = false;
-        defenderselected = false;
-        maul = {
-            name: "maul",
-            healthPoints: 100,
-            attackPower: 8,
-            counterAttackPower: 10,
-        };
-        luke = {
-            name: "luke",
-            healthPoints: 120,
-            attackPower: 5,
-            counterAttackPower: 5,
-        };
-        obi = {
-            name: "obi",
-            healthPoints: 150,
-            attackPower: 8,
-            counterAttackPower: 20,
-        };
-        mace = {
-            name: "mace FLANDERS",
-            healthPoints: 180,
-            attackPower: 15,
-            counterAttackPower: 25,
-        };
-        playerHealthpoints = 0;
-        playerAttack = 0;
-        playerAttackUpdate = 0;
-        playername = "";
-        enemyname = "";
-        enemyCounterAttack = 0;
-        enemyHealthpoints = 0;
-        player = "";
-        defender = "";
-        counterwin = 0;
-        line1 = $("<div>");
-        line2 = $("<div>");
-        $("#boxmaul").appendTo("#playerDiv");
-        $("#boxmaul").css("border-color", "white");
-        $("#boxmaul").css("color","black");
-        $("#boxmaul").css("display", "inline-flex");
-        $("#boxluke").appendTo("#playerDiv");
-        $("#boxluke").css("border-color", "white");
-        $("#boxluke").css("color","black");
-        $("#boxluke").css("display", "inline-flex");
-        $("#boxobi").appendTo("#playerDiv");
-        $("#boxobi").css("border-color", "white");
-        $("#boxobi").css("color","black");
-        $("#boxobi").css("display", "inline-flex");
-        $("#boxmace").appendTo("#playerDiv");
-        $("#boxmace").css("border-color", "white");
-        $("#boxmace").css("color","black");
-        $("#boxmace").css("display", "inline-flex");
-        $("#maulAP").text(maul.healthPoints);
-        $("#lukeAP").text(luke.healthPoints);
-        $("#obiAP").text(obi.healthPoints);
-        $("#maceAP").text(mace.healthPoints);
-        $("#attack").attr("disabled", true);
-        line1.text("Choose your Charater to start the game");
-        $("#defenderDiv").append(line1);*/
+     
         
     })
 });
